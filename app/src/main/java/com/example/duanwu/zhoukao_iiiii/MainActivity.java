1dbf02f0e70139b2fcc89320fcb91b3389674680
gt2454 <2454027056@qq.com>
2019-01-18 15:45:10 +0800

    上传

---
+++ app/src/main/java/com/example/duanwu/zhoukao_iiiii/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.duanwu.zhoukao_iiiii;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
